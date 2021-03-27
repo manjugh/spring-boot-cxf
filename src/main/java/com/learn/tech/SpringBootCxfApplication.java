3ec8da65649ba093016b49fcbe1d3c82be514a79
Manjunatha G H <manjunatha.hanumantappa@evry.com>
2021-03-27 12:47:28 +0530

    First commit

---
+++ src/main/java/com/learn/tech/SpringBootCxfApplication.java
@@ -0,0 +1,13 @@
+package com.learn.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCxfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCxfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/tech/SpringBootCxfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCxfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
